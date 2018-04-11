49ca639725c9b1385840ea675e05cddb3b9cb7f5
Nicholas Romero <nromero@protonmail.com>
2018-04-11 14:58:25 -0400

    sets up asynchronous features, sibling subscribe to observables/component communication

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+// import services
+import { LogService } from '../../services/log.service';
+// import data models/interface
+import { Log } from '../../models/Log';
+
 @Component({
   selector: 'app-log-form',
   templateUrl: './log-form.component.html',
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+
+    // subscribe to selectedLog observable
+    // call from sibling component
+    this.logService.selectedLog.subscribe(log => {
+      if (log.id !== null) {
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    });
   }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 
+// import reactive extensions rxjs
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 // import data models/interface
 import { Log } from '../models/Log';
 
@@ -9,19 +14,28 @@ export class LogService {
   // define properties
   logs: Log[];
 
+  // source
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable(); 
+
+
   constructor() { 
     this.logs = [
-      {id: '1', text: 'Setup devlog git repo', date: new Date
+      {id: '01', text: 'Setup devlog git repo', date: new Date
       ('04/10/2018 15:45:10')},
-      {id: '1', text: 'Generated components', date: new Date
+      {id: '02', text: 'Generated components', date: new Date
       ('04/11/2018 12:54:23')},
-      {id: '1', text: 'Customized log-form', date: new Date
+      {id: '03', text: 'Customized log-form', date: new Date
       ('04/11/2018 13:25:54')}
     ]
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 
 }
