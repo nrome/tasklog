0064a8eaaf1f896c2eb3e6903c97766ada920ba9
Nicholas Romero <nromero@protonmail.com>
2018-04-12 10:43:28 -0400

    completes fully functional CRUD concepts (i.e. add, update, and delete)

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,6 +14,7 @@ export class LogFormComponent implements OnInit {
   id: string;
   text: string;
   date: any;
+  isNew: boolean = true;
 
   constructor(private logService: LogService) { }
 
@@ -23,10 +24,42 @@ export class LogFormComponent implements OnInit {
     // call from sibling component
     this.logService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+
+  }
+
+  onSubmit() {
+    // check if new log
+    if(this.isNew) {
+      // create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      } 
+      // add log
+      this.logService.addLog(newLog);
+    } else {
+        // create log to be updated
+        const updLog = {
+          id: this.id,
+          text: this.text,
+          date: new Date()
+        }
+        // update log
+        this.logService.updateLog(updLog);
+      }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 
